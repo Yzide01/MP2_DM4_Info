8ece8a2bf1d1fe064f2f309915d3f9a9cadffcb5
FlorentinLiu <89586311+FlorentinLiu@users.noreply.github.com>
2024-05-27 21:24:17 +0200

    Update n_dames.c

--- problemes/n_dames.c
+++ problemes/n_dames.c
@@ -91,12 +91,11 @@ char *contrainte_toutes_colonnes(int n ){
         return res;
     }
     //Diagonale -1 -> diagonale qui commence à [1][0]
-char *contrainte_une_diagonale(int i,int n ){
-        
+char *contrainte_une_diagonale(int i,int n ){     
     char **l=malloc((n-abs(i))*sizeof(char*));
 
     
-        if (i<0){
+    if (i<0){
         i= -i ;
         int colonne=0; 
         for(int j=i;j<n;j++){
@@ -105,31 +104,63 @@ char *contrainte_une_diagonale(int i,int n ){
         }
         
         }
-        else{
+    else{
         int ligne=0;
         for(int j=i;j<n;j++){
             l[ligne]=variable(ligne,j);
             ligne++;
         }
-        }
-        
-        
-        int *taille_phi=malloc(sizeof(int));
-        char *phi=au_plus_une(l,(n-abs(i)),taille_phi);
-        for(int j=0;j<n-abs(i);j++){
-            free(l[j]);
-        }
-        
-
-        
-        free(l);
-        free(taille_phi);
-        
-        
-        return phi;
-
-
     }
+        
+        
+    int *taille_phi=malloc(sizeof(int));
+    char *phi=au_plus_une(l,(n-abs(i)),taille_phi);
+    for(int j=0;j<n-abs(i);j++){
+        free(l[j]);
+    }
+        
+
+        
+    free(l);
+    free(taille_phi);   
+    return phi;
+}
+char *contrainte_une_diagonale2(int i,int n ){
+    char **l=malloc((n-abs(i))*sizeof(char*));
+
+    
+    if (i<0){
+        i= -i ;//diagonale partant de la colonne tout à droite
+        int colonne=n-1; 
+        for(int j=i;j<n;j++){
+            l[j-i]=variable(j,colonne);
+            colonne--;
+        }
+        
+        }
+    else{
+        int ligne=0;
+        for(int j=n-i-1;j>-1;j--){
+            l[ligne]=variable(ligne,j);
+            ligne++;
+        }
+    }
+        
+        
+    int *taille_phi=malloc(sizeof(int));
+    char *phi=au_plus_une(l,(n-abs(i)),taille_phi);
+    for(int j=0;j<n-abs(i);j++){
+        free(l[j]);
+    }
+        
+
+        
+    free(l);
+    free(taille_phi);   
+    return phi;
+
+
+}
 
 char *contrainte_toute_diagonale(int n ){
     char** l = malloc(((2*n)-1)*sizeof(char*));
@@ -157,20 +188,49 @@ char *contrainte_toute_diagonale(int n ){
     return res;
 }
 
+char *contrainte_toute_diagonale2(int n ){
+    char** l = malloc(((2*n)-1)*sizeof(char*));
+        
+    l[0]=contrainte_une_diagonale2(0,n);
+
+        
+    for (int i = -1; i > (-n); i--){
+        //printf("%d\n",i);
+        l[-i] = contrainte_une_diagonale2(i,n);
+    }
+        
+    for(int i=1;i<n ;i++){
+        //printf("%d\n",i);
+        l[i+n-1]=contrainte_une_diagonale2(i,n);
+        //printf("%s\n\n",l[i+n-1]);
+    }
+    char* res = separation_et (l,(2*n)-1);
+    
+    for(int j=0;j<2*n-1;j++){
+        free(l[j]);
+    }
+        
+    free(l);
+    return res;
+}
+
     void gen_formule_n_dames(int n,char* filename){
     char *contrainte1=contrainte_toutes_lignes(n);
     char *contrainte2=contrainte_toutes_colonnes(n);
     char *contrainte3=contrainte_toute_diagonale(n);
+    char *contrainte4=contrainte_toute_diagonale2(n);
 
-    char **l=malloc(3*sizeof(char*));
+    char **l=malloc(4*sizeof(char*));
     l[0]=contrainte1;
     l[1]=contrainte2;
     l[2]=contrainte3;
+    l[3]=contrainte4;
 
-    char *formule_resolution_pb_n_dames=separation_et(l,3);;
+    char *formule_resolution_pb_n_dames=separation_et(l,4);;
     free(l[0]);
     free(l[1]);
     free(l[2]);
+    free(l[3]);
     free(l);
     
     FILE *fichier=fopen(filename,"w");
@@ -185,6 +245,6 @@ char *contrainte_toute_diagonale(int n ){
 
     int main(){
         gen_formule_n_dames(8,"test.txt");
-        
+
         return 0;
     }
